f717d6f57f494331a334a5620e95bb205bb80d4c
satyam-skr <satyamrai0010@gmail.com>
2025-08-11 22:07:48 +0530

    signup route

--- src/helpers/sendVerificationEmail.ts
+++ src/helpers/sendVerificationEmail.ts
@@ -1,13 +1,22 @@
 import { resend } from "@/lib/resend";
+import VerificationEmail from "../../emails/VerificationEmail";
+import { ApiResponse } from "@/types/ApiResponse";
 
-
-
-
-
-
-
-
-
-
-
-
+export async function sendVerificationEmail(
+  email: string,
+  username: string,
+  verifyCode: string
+): Promise<ApiResponse> {
+  try {
+    const { data, error } = await resend.emails.send({
+      from: "onboarding@resend.dev",
+      to: email,
+      subject: "Anonymous gossip | Verification code",
+      react: VerificationEmail({ username, otp: verifyCode}),
+    });
+    return { success: true, message: "verification email sent successfully" };
+  } catch (emailError) {
+    console.error("Error sending verification email : ", emailError);
+    return { success: false, message: "failed to send verification email" };
+  }
+}
